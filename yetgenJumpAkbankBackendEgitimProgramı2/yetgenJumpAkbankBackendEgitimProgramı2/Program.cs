fdc7f6cba84e0348fdcc2793b11438137622f28c
damlanurozcann <102881538+damlanurozcann@users.noreply.github.com>
2023-08-27 17:17:15 +0300

    Yetgen-backend-assessment-2

---
+++ yetgenJumpAkbankBackendEgitimProgramı2/yetgenJumpAkbankBackendEgitimProgramı2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace yetgenJumpAkbankBackendEgitimProgramı2
+{
+    class Program
+    {
+        private const string Pattern = @"(?<=[.,;?!])";
+
+        static string TextControl(string text)
+        {
+            string newText = Regex.Replace(text, Pattern, " ");
+            string[] words = newText.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i == 0 || words[i - 1].EndsWith(".") || words[i - 1].EndsWith("!") || words[i - 1].EndsWith("?"))
+                {
+                    words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+                }
+            }
+
+            string resultNewText = string.Join(" ", words);
+            return resultNewText;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter text");
+            string enterText = Convert.ToString(Console.ReadLine());
+            string result = TextControl(enterText);
+            Console.WriteLine("Result:");
+            Console.WriteLine(result);
+        }
+    }
+}
